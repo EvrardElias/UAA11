afb01c8a9fb76cab3a35b3e208ab6b744dca473b
Evrard Elias <190353@CCM-Asty-Moulin.local>
2023-04-04 11:59:12 +0200

    Travail sur les matrice

---
+++ Matrice/5TTI_Matrice_Evrard/5TTI_Matrice_Evrard/Program.cs
@@ -0,0 +1,251 @@
+﻿using System;
+
+namespace _5TTI_Matrice_Evrard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Traitement outils = new Traitement();
+            string choix; ///Reprend le choix dans le menu
+            int[,] matriceUn; ///Matrice d'entier n°1 pour les calcul et utilisé pour la génération aléatoire 
+            int nbrLigne; ///Nombre de ligne dans la matrice
+            int nbrColonne;///Nombre de colonne dans la matrice
+            string chaine;///Chaine de caractère avec la valeur des matrices afin de pouvoir l'afficher
+            int[,] matriceDeux; ///Matrice d'entier n°1 pour les calcul
+            bool additionnable; ///Contient la valeur True/False et en fonction de sa valeur permet d'afficher le resultat ou le message d'erreur
+            int[,] resultat; ///Matrice reprenant la somme ou le produit des calcule effectués
+            bool multipliable; ///Contient la valeur True/False et en fonction de sa valeur permet d'afficher le resultat ou le message d'erreur
+            string newTraitement; ///Contient la valeur de si l'utilisateur veut recommencer avec un nouveau traitement
+            string recommencer; ///Si la valeur pour les dimensions vaut 0 alors recommencer 
+
+            do
+            {
+
+                do
+                {
+
+                    Console.WriteLine("Quelle opération voulez vous faire ? \nTapez 1 : Générer une matrice aléatoire. \nTapez 2 : Additionner 2 matrices. \nTapez 3 : Multiplier 2 matrices.");
+                    choix = Console.ReadLine();
+
+                    if (choix == "1") ///Génération aléatoire
+                    {
+                        do
+                        {
+                            outils.LireEntier("Combien de ligne voulez-vous dans la matrice ?", out nbrLigne);
+                            if (nbrLigne == 0)
+                            {
+                                Console.WriteLine("Le nombre de ligne ne peut pas valoir 0");
+                                recommencer = "Oui";
+                            }
+                            else
+                            {
+                                recommencer = "Non";
+                            }
+                        } while (recommencer == "Oui");
+
+                        do
+                        {
+                            outils.LireEntier("Combien de colonne voulez-vous dans la matrice ?", out nbrColonne);
+                            if (nbrColonne == 0)
+                            {
+                                Console.WriteLine("Le nombre de colonne ne peut pas valoir 0");
+                                recommencer = "Oui";
+                            }
+                            else
+                            {
+                                recommencer = "Non";
+                            }
+                            
+                        } while (recommencer == "Oui");
+                        outils.GenererAleatoireMatrice(nbrLigne, nbrColonne, out matriceUn);
+                        outils.ConcaténationMatrice(matriceUn, out chaine);
+                        Console.WriteLine("Voici votre matrice : \n" + chaine);
+                    }
+                    else if (choix == "2") ///L'addition
+                    {
+                        do
+                        {
+                            outils.LireEntier("Combien de ligne voulez-vous dans la matrice n°1 ?", out nbrLigne);
+                            if (nbrLigne == 0)
+                            {
+                                Console.WriteLine("Le nombre de ligne ne peut pas valoir 0");
+                                recommencer = "Oui";
+                            }
+                            else
+                            {
+                                recommencer = "Non";
+                            }
+                            
+                        } while (recommencer == "Oui");
+
+                        do
+                        {
+                            outils.LireEntier("Combien de colonne voulez-vous dans la matrice n°1 ?", out nbrColonne);
+                            if (nbrColonne == 0)
+                            {
+                                Console.WriteLine("Le nombre de colonne ne peut pas valoir 0");
+                                recommencer = "Oui";
+                            }
+                            else
+                            {
+                                recommencer = "Non";
+                            }
+
+                        } while (recommencer == "Oui");
+
+                        outils.GenererAleatoireMatrice(nbrLigne, nbrColonne, out matriceUn);
+
+                        do
+                        {
+                            outils.LireEntier("Combien de ligne voulez-vous dans la matrice n°2 ?", out nbrLigne);
+                            if (nbrLigne == 0)
+                            {
+                                Console.WriteLine("Le nombre de ligne ne peut pas valoir 0");
+                                recommencer = "Oui";
+                            }
+                            else
+                            {
+                                recommencer = "Non";
+                            }
+
+                        } while (recommencer == "Oui");
+
+                        do
+                        {
+                            outils.LireEntier("Combien de colonne voulez-vous dans la matrice n°2 ?", out nbrColonne);
+                            if (nbrColonne == 0)
+                            {
+                                Console.WriteLine("Le nombre de colonne ne peut pas valoir 0");
+                                recommencer = "Oui";
+                            }
+                            else
+                            {
+                                recommencer = "Non";
+                            }
+
+                        } while (recommencer == "Oui");
+                        outils.GenererAleatoireMatrice(nbrLigne, nbrColonne, out matriceDeux);
+                        outils.ConcaténationMatrice(matriceUn, out chaine);
+                        Console.WriteLine("Voici votre première matrice : \n" + chaine);
+                        outils.ConcaténationMatrice(matriceDeux, out chaine);
+                        Console.WriteLine("Voici votre deuxième matrice : \n" + chaine);
+                        outils.AdditionMatrice(matriceUn, matriceDeux, out additionnable, out resultat);
+                        if (additionnable == false)
+                        {
+                            Console.WriteLine("Vos matrice ne peuvent pas être additionnés");
+                        }
+                        else
+                        {
+                            outils.ConcaténationMatrice(resultat, out chaine);
+                            Console.WriteLine("Voici la somme de vos matrices : \n" + chaine);
+                        }
+
+                    }
+                    else if (choix == "3") ///La multiplication
+                    {
+                        do
+                        {
+                            outils.LireEntier("Combien de ligne voulez-vous dans la matrice n°1 ?", out nbrLigne);
+                            if (nbrLigne == 0)
+                            {
+                                Console.WriteLine("Le nombre de ligne ne peut pas valoir 0");
+                                recommencer = "Oui";
+                            }
+                            else
+                            {
+                                recommencer = "Non";
+                            }
+
+                        } while (recommencer == "Oui");
+
+                        do
+                        {
+                            outils.LireEntier("Combien de colonne voulez-vous dans la matrice n°1 ?", out nbrColonne);
+                            if (nbrColonne == 0)
+                            {
+                                Console.WriteLine("Le nombre de colonne ne peut pas valoir 0");
+                                recommencer = "Oui";
+                            }
+                            else
+                            {
+                                recommencer = "Non";
+                            }
+
+                        } while (recommencer == "Oui");
+
+                        outils.GenererAleatoireMatrice(nbrLigne, nbrColonne, out matriceUn);
+
+                        do
+                        {
+                            outils.LireEntier("Combien de ligne voulez-vous dans la matrice n°2 ?", out nbrLigne);
+                            if (nbrLigne == 0)
+                            {
+                                Console.WriteLine("Le nombre de ligne ne peut pas valoir 0");
+                                recommencer = "Oui";
+                            }
+                            else
+                            {
+                                recommencer = "Non";
+                            }
+
+                        } while (recommencer == "Oui");
+
+                        do
+                        {
+                            outils.LireEntier("Combien de colonne voulez-vous dans la matrice n°2 ?", out nbrColonne);
+                            if (nbrColonne == 0)
+                            {
+                                Console.WriteLine("Le nombre de colonne ne peut pas valoir 0");
+                                recommencer = "Oui";
+                            }
+                            else
+                            {
+                                recommencer = "Non";
+                            }
+
+                        } while (recommencer == "Oui");
+                        outils.GenererAleatoireMatrice(nbrLigne, nbrColonne, out matriceDeux);
+                        outils.ConcaténationMatrice(matriceUn, out chaine);
+                        Console.WriteLine("Voici votre première matrice : \n" + chaine);
+                        outils.ConcaténationMatrice(matriceDeux, out chaine);
+                        Console.WriteLine("Voici votre deuxième matrice : \n" + chaine);
+                        outils.MultiplicationMatrice(matriceUn, matriceDeux, out multipliable, out resultat);
+                        if (multipliable == false)
+                        {
+                            Console.WriteLine("Vos matrice ne peuvent pas être multipliés");
+                        }
+                        else
+                        {
+                            outils.ConcaténationMatrice(resultat, out chaine);
+                            Console.WriteLine("Voici le produit de vos matrices : \n" + chaine);
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Veuillez rentrer uniquement un chiffre entre 1 et 3 s'il vous plait");
+                        choix = "Non";
+                    }
+
+                } while (choix == "Non");
+
+                do
+                {
+                    Console.WriteLine("Voulez-vous faire un nouveau traitement ? Oui/Non");
+                    newTraitement = Console.ReadLine();
+                    if (newTraitement != "Oui" || newTraitement != "Non")
+                    {
+                        Console.WriteLine("Veuillez rentrer uniquement Oui ou Non");
+                        recommencer = "Oui";
+                    }
+                    else
+                    {
+                        recommencer = "Non";
+                    }
+                } while (recommencer == "Oui");
+
+            } while (newTraitement == "Oui");
+        }
+    }
+}
---
+++ Matrice/5TTI_Matrice_Evrard/5TTI_Matrice_Evrard/Traitement.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5TTI_Matrice_Evrard
+{
+    public struct Traitement
+    {
+        /// <summary>
+        /// nbrLigne et nbrColonne doivent être entier et non nul
+        /// 
+        /// Obtenir une matrice avec des valeur aléatoires
+        /// </summary>
+        /// <param name="nbrLigne">nombre de ligne dans la matrice</param>
+        /// <param name="nbrColonne">nombre de colonne dans la matrice</param>
+        /// <param name="matrice">la matrice contenant toute les valeurs</param>
+        public void GenererAleatoireMatrice (int nbrLigne, int nbrColonne, out int[,] matrice)
+        {
+            matrice = new int[nbrLigne, nbrColonne];
+            Random valeur = new Random();
+            for (int compteurLigne = 0; compteurLigne <= nbrLigne - 1; compteurLigne++)
+            {
+                for (int compteurColonne = 0; compteurColonne <= nbrColonne - 1; compteurColonne++)
+                {
+                    matrice[compteurLigne, compteurColonne] = valeur.Next(0, 21);
+                }
+            }
+        }
+        /// <summary>
+        /// Les matrices doivent être crées au préalable
+        /// 
+        /// Concaténer une matrice en chaine de caractère
+        /// </summary>
+        /// <param name="matrice">matrice d’entier</param>
+        /// <param name="chaine">chaine de caractère résultant d’une concaténation d’une matrice</param>
+        public void ConcaténationMatrice (int[,] matrice, out string chaine)
+        {
+            chaine = "";
+            for (int compteurLigne = 0; compteurLigne <= matrice.GetLength(0) - 1; compteurLigne++)
+            {
+                for (int compteurColonne = 0; compteurColonne <= matrice.GetLength(1) - 1; compteurColonne++)
+                {
+                    chaine = chaine + matrice[compteurLigne, compteurColonne] + " ";
+                }
+                chaine = chaine + "\n";
+            }
+        }
+        /// <summary>
+        /// Les matrices doivent être créés au préalable
+        /// 
+        /// Vérifier si on peut additionner 2 matrices et le faire si c’est le cas
+        /// </summary>
+        /// <param name="matriceUn">Matrice d’entier</param>
+        /// <param name="matriceDeux">Matrice d’entier</param>
+        /// <param name="additionnable">Vérifie que les 2 matrices peuvent être additionnés</param>
+        /// <param name="resultat">Matrice contenant l’addition des autres matrices</param>
+        public void AdditionMatrice (int[,] matriceUn, int[,] matriceDeux, out bool additionnable, out int[,] resultat)
+        {
+            resultat = new int[matriceUn.GetLength(0), matriceUn.GetLength(1)];
+
+            if (matriceUn.GetLength(0) == matriceDeux.GetLength(0) && matriceUn.GetLength(1) == matriceDeux.GetLength(1))
+            {
+                additionnable = true;
+                for (int compteurLigne = 0; compteurLigne <= matriceUn.GetLength(0) - 1; compteurLigne++)
+                {
+                    for (int compteurColonne = 0; compteurColonne <= matriceUn.GetLength(1) - 1; compteurColonne++)
+                    {
+                        resultat[compteurLigne, compteurColonne] = 0;
+                        resultat[compteurLigne, compteurColonne] = matriceUn[compteurLigne, compteurColonne] + matriceDeux[compteurLigne, compteurColonne];
+                    }
+                }
+            }
+            else
+            {
+                additionnable = false;
+            }
+        }
+        /// <summary>
+        /// Les matrices doivent être crées au préalable
+        /// 
+        /// Verifier si on peut multiplier les 2 matrices et le faire si c’est le cas
+        /// </summary>
+        /// <param name="matriceUn">Matrice d’entier</param>
+        /// <param name="matriceDeux">Matrice d’entier</param>
+        /// <param name="multipliable">Vérifie que les 2 matrices peuvent être multipliés</param>
+        /// <param name="resultat">Matrice contenant la multiplication des autres matrices</param>
+        public void MultiplicationMatrice (int[,] matriceUn, int[,] matriceDeux, out bool multipliable, out int[,] resultat)
+        {
+            resultat = new int[matriceUn.GetLength(0), matriceDeux.GetLength(1)];
+
+            if (matriceUn.GetLength(1) == matriceDeux.GetLength(0))
+            {
+                multipliable = true;
+                for (int compteurLigne = 0; compteurLigne <= matriceUn.GetLength(0) - 1; compteurLigne++)
+                {
+                    for (int compteurColonne = 0; compteurColonne <= matriceDeux.GetLength(1) - 1; compteurColonne++)
+                    {
+                        resultat[compteurLigne, compteurColonne] = 0;
+                        for (int compteurLigneColonne = 0; compteurLigneColonne <= matriceUn.GetLength(1) - 1; compteurLigneColonne++)
+                        {
+                            resultat[compteurLigne, compteurColonne] = resultat[compteurLigne, compteurColonne] + matriceUn[compteurLigne, compteurLigneColonne] * matriceDeux[compteurLigneColonne, compteurColonne];
+                        }
+                    }
+                }
+            }
+            else
+            {
+                multipliable = false;
+            }
+        }
+        /// <summary>
+        /// La question doit être ecrité à l'avance
+        /// 
+        /// Verifier si la valeur rentrée par l'utilisateur est un entier
+        /// </summary>
+        /// <param name="question">Question posé par la console</param>
+        /// <param name="n">Renvoie la réponse de l'utilisateur si il s'agit d'un nombre sinon renvoie un message d'erreur</param>
+        public void LireEntier(string question, out int n)
+        {
+            string nUser;
+            Console.Write(question);
+            nUser = Console.ReadLine();
+            while (!int.TryParse(nUser, out n))
+            {
+                Console.WriteLine("Attention! Vous ne devez taper qu'un nombre entier !");
+                nUser = Console.ReadLine();
+            }
+        }
+    }
+}
