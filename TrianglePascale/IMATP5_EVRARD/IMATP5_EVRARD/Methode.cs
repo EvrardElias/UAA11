09be3f709f27ee0317c66db645340bf4335ab860
Evrard Elias <190353@CCM-Asty-Moulin.local>
2023-04-18 12:20:12 +0200

    Examen matrice

---
+++ TrianglePascale/IMATP5_EVRARD/IMATP5_EVRARD/Methode.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMATP5_EVRARD
+{
+    public struct Methode
+    {
+        public void RenvoieTriangle(int tailleTriangle, out int[,] triangle)
+        {
+            triangle = new int[tailleTriangle, tailleTriangle];
+            triangle[0, 0] = 1;
+            for (int nbrLigne = 1; nbrLigne <= tailleTriangle - 1; nbrLigne++)
+            {
+                for (int nbrColonne = 0; nbrColonne <= tailleTriangle - 1; nbrColonne++)
+                {
+                    triangle[nbrLigne, nbrColonne] = 0;
+                    if (nbrColonne == 1)
+                    {
+                        triangle[nbrLigne, nbrColonne] = 1;
+                    }
+                    else
+                    {
+                        triangle[nbrLigne, nbrColonne] = triangle[nbrLigne - 1, nbrColonne - 1] + triangle[nbrLigne - 1, nbrColonne - 1];
+                    }
+                }
+            }
+        }
+
+        public void ConstructionChaine(int tailleTriangle, int[,] triangle, out string chaine)
+        {
+            chaine = "";
+            for (int nbrColonne = 0; nbrColonne <= tailleTriangle - 1; nbrColonne++)
+            {
+                chaine = chaine + triangle[tailleTriangle - 1, nbrColonne] + "a^" + (tailleTriangle - nbrColonne) + "b^" + nbrColonne + "+";
+            }
+        }
+
+        public void LireEntier(string question, out int n)
+        {
+            string nUser;
+            Console.Write(question);
+            nUser = Console.ReadLine();
+            while (!int.TryParse(nUser, out n))
+            {
+                Console.WriteLine("Attention! Vous ne devez taper qu'un nombre entier !");
+                nUser = Console.ReadLine();
+            }
+        }
+
+        public void ConcaténationMatrice(int[,] matrice, out string chaine)
+        {
+            chaine = "";
+            for (int compteurLigne = 0; compteurLigne <= matrice.GetLength(0) - 1; compteurLigne++)
+            {
+                for (int compteurColonne = 0; compteurColonne <= matrice.GetLength(1) - 1; compteurColonne++)
+                {
+                    chaine = chaine + matrice[compteurLigne, compteurColonne] + " ";
+                }
+                chaine = chaine + "\n";
+            }
+        }
+    }
+}
---
+++ TrianglePascale/IMATP5_EVRARD/IMATP5_EVRARD/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace IMATP5_EVRARD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methode outils = new Methode();
+            int tailleTriangle;
+            bool pasZero = false;
+            int[,] triangle;
+            string conca;
+            string afficher;
+            string chaine;
+            bool ouiNon = false;
+            string recommencer;
+            bool restart = false;
+
+            do
+            {
+                do
+                {
+                    outils.LireEntier("Que voulez vous comme taille pour votre matrice ?", out tailleTriangle);
+                    if (tailleTriangle == 0)
+                    {
+                        Console.WriteLine("Veuillez rentrer une valeur non nul");
+                        pasZero = true;
+                    }
+                } while (pasZero == true);
+                outils.RenvoieTriangle(tailleTriangle, out triangle);
+                outils.ConcaténationMatrice(triangle, out conca);
+                Console.WriteLine("Voici votre triangle de Pascale: " + conca);
+                do
+                {
+                    Console.WriteLine("Voulez vous qu'on vous affiche le développement ? (Oui/Non)");
+                    afficher = Console.ReadLine();
+                    if (afficher == "Oui")
+                    {
+                        outils.ConstructionChaine(tailleTriangle, triangle, out chaine);
+                        Console.WriteLine("Voici le developpement : " + chaine);
+                        ouiNon = true;
+                    }
+                    else if (afficher == "Non")
+                    {
+                        ouiNon = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Veuillez repondre uniquement par Oui ou Non");
+                        ouiNon = false;
+                    }
+                } while (ouiNon == false);
+                do
+                {
+                    Console.WriteLine("Voulez-vous recommencer ? Oui/Non");
+                    recommencer = Console.ReadLine();
+                    if (recommencer == "Oui")
+                    {
+                        restart = true;
+                        ouiNon = true;
+                    }
+                    else if (recommencer == "Non")
+                    {
+                        restart = false;
+                        ouiNon = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Veuillez repondre uniquement par Oui ou Non");
+                        ouiNon = false;
+                    }
+                } while (ouiNon == false);
+
+            } while (restart == true);
+        }
+    }
+}
